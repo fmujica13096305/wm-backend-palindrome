b06ecc82c66692849a5a7d6e59fc5eabb744b688
Fernando.Mujica <fernando.mujica@sovos.com>
2021-02-09 01:07:38 -0300

    Adding Selenium test cases

---
+++ src/test/java/com/walmart/integrationTest/SeleniumWalmartTestCase.java
@@ -0,0 +1,86 @@
+package com.walmart.integrationTest;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+
+public class SeleniumWalmartTestCase {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver2.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testWalmartTestCase() throws Exception {
+        driver.get("http://localhost:3000/");
+        driver.findElement(By.name("text")).click();
+        driver.findElement(By.name("text")).clear();
+        driver.findElement(By.name("text")).sendKeys("181");
+        driver.findElement(By.id("buttonID")).click();
+        driver.findElement(By.id("onSaleID")).click();
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver, 5000); // 5 seconds timeout
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
